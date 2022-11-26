8255620445f1090795af961861b308bfd6b0663f
kjsj20 <kjsj20@naver.com>
2022-11-26 17:15:15 +0900

    initial src

---
+++ src/main/java/com/example/crazyProject/CrazyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.crazyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crazyProject/CrazyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crazyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

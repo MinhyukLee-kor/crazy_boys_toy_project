524051a99c87ecbfdfbfb1c057670128d8380884
minhyuk <kazesky93@gmail.com>
2022-12-03 17:21:52 +0900

    select문 직접 입력

--- src/main/java/com/example/crazyProject/postgresSQLRunner.java
+++ src/main/java/com/example/crazyProject/postgresSQLRunner.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.Scanner;
 
 @Component
 public class postgresSQLRunner implements ApplicationRunner {
@@ -22,25 +23,35 @@ public class postgresSQLRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        String sql="";
         try (Connection connection = dataSource.getConnection()) {
-            System.out.println(dataSource.getClass());
-            System.out.println(connection.getMetaData().getURL());
-            System.out.println(connection.getMetaData().getUserName());
-
+//            System.out.println(dataSource.getClass());
+//            System.out.println(connection.getMetaData().getURL());
+//            System.out.println(connection.getMetaData().getUserName());
             Statement statement = connection.createStatement();
 
-            ResultSet rs = statement.executeQuery("SELECT * FROM user_info");
-            ResultSetMetaData rsmd = rs.getMetaData();
-            System.out.println("querying SELECT * FROM user_info");
-            int columnsNumber = rsmd.getColumnCount();
-            while (rs.next()) {
-                for (int i = 1; i <= columnsNumber; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = rs.getString(i);
-                    System.out.print(rsmd.getColumnName(i)+":"+columnValue);
+            System.out.println("쿼리를 입력해라. 종료하려면 END 를 입력해라");
+            Scanner scan = new Scanner(System.in);
+            sql = scan.nextLine();
+            if(!(sql.equals("END"))) {
+                ResultSet rs = statement.executeQuery(sql);
+                ResultSetMetaData rsmd = rs.getMetaData();
+                System.out.println("입력한 쿼리 : "+sql);
+                int columnsNumber = rsmd.getColumnCount();
+                while (rs.next()) {
+                    for (int i = 1; i <= columnsNumber; i++) {
+                        if (i > 1) System.out.print(",  ");
+                        String columnValue = rs.getString(i);
+                        System.out.print(rsmd.getColumnName(i) + ":" + columnValue);
+                    }
+                    System.out.println("");
                 }
-                System.out.println("");
+            } else {
+                System.out.println("사요나라~");
             }
+        } catch (Exception e) {
+            System.out.println("입력한 쿼리 : "+sql+" 는 쿼리가아니라 카레임ㅋㅋ. 다시하셈");
+            run(args);
         }
     }
 }

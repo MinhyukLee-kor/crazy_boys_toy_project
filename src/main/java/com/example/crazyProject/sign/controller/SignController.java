c7d0a8ea63fd76ec4154c23546601b53aee4c0c5
minhyuk <kazesky93@gmail.com>
2023-02-04 23:38:15 +0900

    주석 수정

--- src/main/java/com/example/crazyProject/sign/controller/SignController.java
+++ src/main/java/com/example/crazyProject/sign/controller/SignController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 로그인관련
+ * @author minhyuk
+ * @version 2023.02.04 최초작성
+ * */
 @RestController
 @RequestMapping(value = "/sign/")
 public class SignController {
@@ -16,18 +21,30 @@ public class SignController {
     SignService signService;
 
     /**
+     * id중복확인
      * @author minhyuk
-     * @return int (0:가입가능,1:가입불가능)*/
+     * @return int (0:가입가능,1:가입불가능)
+     * */
     @RequestMapping(value = "idCheck", method = RequestMethod.POST)
     public int idCheck(@RequestBody String userId) {
         return signService.idCheck(userId);
     }
 
+    /**
+     * 회원가입
+     * @author minhyuk
+     * @return int (1:가입성공)
+     * */
     @RequestMapping(value = "signUp", method = RequestMethod.POST)
     public int signUp(@RequestBody User user) {
         return signService.signUp(user);
     }
 
+    /**
+     * 로그인
+     * @author minhyuk
+     * @return String (페이지이동)
+     * */
     @RequestMapping(value = "signIn", method = RequestMethod.POST)
     public String signIn(@RequestBody User user) {
         String userId = user.getUser_id();

72652a3d09ceaca986dcd9bf70a737d2ef85bc5f
whitehairdevjs <jsuserwork20@gmail.com>
2023-02-06 23:13:53 +0900

    Access-Control-Allow-Origin Error Catch

---
+++ src/main/java/com/example/crazyProject/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.crazyProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000") //접근할 Origin URL 설정
+                .allowedMethods("GET", "POST"); //허용할 HTTP method 지정
+    }
+}
